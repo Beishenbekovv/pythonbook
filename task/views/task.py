efb4617010ed1948fc983700a067d8219251051f
Beishenbekovv <150322387+Beishenbekovv@users.noreply.github.com>
2023-12-08 18:03:11 +0600

    hw students APIview

--- task/views/task.py
+++ task/views/task.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
+from rest_framework.generics import RetrieveAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from rest_framework.response import Response
-from task.models.task import *
-from task.serializers.task import *
+from task.models import Task
+from task.serializers import *
 
 
 class TaskDetailAPIView(APIView):
@@ -9,30 +10,12 @@ class TaskDetailAPIView(APIView):
         task_object = Task.objects.get(pk=kwargs.get("pk"))
         serializer = TaskSerializer(instance=task_object)
         return Response(serializer.data)
-    
-    def put(self, request, *args, **kwargs):
-        task_object= Task.objects.get(pk=kwargs.get("pk"))
 
-        serializer = TaskUpdateSerializer(
-            instance=task_object,
-            data=request.data
-        )
-        if serializer.is_valid():
-            task_object=serializer.save()
-            return Response(serializer.data, 202)
-        else:
-            return Response(serializer.errors, 400)
-    
-    def delete(self, request, *args, **kwargs):
-        task_object= Task.objects.get(pk=kwargs.get("pk"))  
-        task_object.delete() 
-        return Response("Запись удалена", 204)   
-    
+
 class TasksView(APIView):
     def get(self, request, *args, **kwargs):
         task_list = Task.objects.all()
         serializer = TaskSerializer(task_list, many=True)
-        # json_data = user_serializer.data
         return Response(serializer.data)
     
     def post(self, request, *args, **kwargs):
@@ -43,8 +26,7 @@ class TasksView(APIView):
             return Response("Успешно создано", 201)
         else:
             return Response(serializer.error_messages, 400)
-    
-    
+        
 class AnswersView(APIView):
     def get(self, request, *args, **kwargs):
         answer_list = Answer.objects.all()
@@ -73,4 +55,13 @@ class AnswerDetailAPIView(APIView):
     def delete(sdelf, request, *args, **kwargs):
         answer_object= Answer.objects.get(pk=kwargs.get("pk"))  
         answer_object.delete()
-    
+        
+
+# class TaskGenericDetailAPIView(RetrieveAPIView):
+class TaskGenericDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TasksGenericView(ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskCreateSerializer
